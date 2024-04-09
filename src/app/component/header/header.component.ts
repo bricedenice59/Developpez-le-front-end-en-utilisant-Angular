541d306a35ef90d094233750cdc027486d472881
Brice Grenard <brice.grenard@gmail.com>
2024-04-09 03:57:31 +0200

    Fix warnings and errors logged in the console because of the wrong input image size, remove directive NgOptimizedImage and use classic src attribute.

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
-import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
     RouterLink,
-    NgOptimizedImage
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
